eba4b8fc3f9dac3df7bf5cf2c4d6a10b64295527
picklewagon <josh@picklewagon.com>
2017-02-13 05:37:01 +0000

    Initial commit

---
+++ src/UserSettingsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Picklewagon\UserSettings;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
